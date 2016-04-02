5841c802eb409630a5ec114afa6d78e0d0513869
architMahto <architmahto@cmail.carleton.ca>
2016-04-02 17:25:31 -0600

    Added all technologies and technical-skills for /technical-skills page.

--- public/javascripts/controllers/technicalSkillsController.js
+++ public/javascripts/controllers/technicalSkillsController.js
@@ -7,5 +7,59 @@
   function technicalSkillsController() {
     var technicalSkillsCtrl = this;
 
+    technicalSkillsCtrl.skills = [
+      {
+        category  : "Programming Languages",
+        techs     : ["C", "C++", "C#", "Java", "PHP", "Prolog", "Python", "Ruby", "Scheme", "SQL", "UML"]
+      },
+      {
+        category  : "Markup Languages",
+        techs     : ["HTML", "LaTeX", "Markdown"]
+      },
+      {
+        category  : "Style Sheet Languages",
+        techs     : ["CSS"]
+      },
+      {
+        category  : "Source Code Management Systems",
+        techs     : ["Git", "SVN"]
+      },
+      {
+        category  : "Database Management Systems",
+        techs     : ["MySQL", "NoSQL"]
+      },
+      {
+        category  : "Operating Systems",
+        techs     : ["Android OS", "Linux/Unix", "Microsoft Windows", "Mac OSX"]
+      },
+      {
+        category  : "Software",
+        techs     : ["Eclipse", "Microsoft Visual Studio", "Qt Creator", "XCode", "Unity"]
+      },
+      {
+        category  : "Text Editors",
+        techs     : ["Atom", "Brackets", "Emacs", "Geany", "Notepad++", "Sublime Text", "Vim"]
+      },
+      {
+        category  : "JavaScript Libraries",
+        techs     : ["AngularJS", "Backbone.js", "Express.js", "JQuery", "Node.js"]
+      },
+      {
+        category  : "Web Frameworks",
+        techs     : ["Bootstrap", "Django", "Google Web Toolkit", "Materialize", "Spring"]
+      },
+      {
+        category  : "Testing Frameworks",
+        techs     : ["Cucumber", "JUnit", "Jasmine", "Selenium"]
+      },
+      {
+        category  : "Programming Paradigms",
+        techs     : ["Functional", "Imperative", "Object-Oriented", "Procedural"]
+      },
+      {
+        category  : "Software Development Processes",
+        techs     : ["Agile", "Behavior-Driven-Development", "Scrum", "Test-Driven-Development"]
+      }
+    ]
   }
 })();
