334f8bb058599481b52c974bb5543a09c19213f2
architMahto <architmahto@cmail.carleton.ca>
2016-04-01 22:48:31 -0600

    Created views for about page, projects page, technical-skills page, soft-skills page, history page, education page, and achievements page. Modified app.js to include routes for all views.

--- public/app.js
+++ public/app.js
@@ -12,6 +12,34 @@
         url: '/',
         templateUrl: 'views/home.html'
       })
+      .state('about',{
+        url: '/about',
+        templateUrl: 'views/about.html'
+      })
+      .state('projects',{
+        url: '/projects',
+        templateUrl: 'views/projects.html'
+      })
+      .state('technical-skills',{
+        url: '/technical-skills',
+        templateUrl: 'views/technical-skills.html'
+      })
+      .state('soft-skills',{
+        url: '/soft-skills',
+        templateUrl: 'views/soft-skills.html'
+      })
+      .state('history',{
+        url: '/history',
+        templateUrl: 'views/history.html'
+      })
+      .state('education',{
+        url: '/education',
+        templateUrl: 'views/education.html'
+      })
+      .state('achievements',{
+        url: '/achievements',
+        templateUrl: 'views/achievements.html'
+      })
       $urlRouterProvider.otherwise('/');
   }
 })();
