6a8609a45c0f5235a758e4b310b95430a3a759fa
architMahto <architmahto@cmail.carleton.ca>
2016-04-02 18:37:20 -0600

    Added content to /soft-skills page.

--- public/javascripts/controllers/softSkillsController.js
+++ public/javascripts/controllers/softSkillsController.js
@@ -7,5 +7,23 @@
   function softSkillsController() {
     var softSkillsCtrl = this;
 
+    softSkillsCtrl.skills = [
+      {
+        category : "Communication Skills",
+        types    : ["Documentation", "Humorously Speaking Skills", "Interpersonal Communication", "Networking", "Storytelling", "Toastmaster"]
+      },
+      {
+        category : "Analytical Skills",
+        types    : ["Debugging", "Problem Solver"]
+      },
+      {
+        category : "Leadership Skills",
+        types    : ["Mentoring"]
+      },
+      {
+        category : "Languages",
+        types    : ["English", "French", "Hindi"]
+      }
+    ]
   }
 })();
