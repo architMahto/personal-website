7982dd2daeff83f4e26e275fc85887e878b6a0f4
architMahto <architmahto@cmail.carleton.ca>
2016-04-03 21:57:23 -0600

    Made changes to social media glyphicons.

--- public/javascripts/controllers/homeController.js
+++ public/javascripts/controllers/homeController.js
@@ -9,23 +9,45 @@
 
     homeCtrl.socialMedisSites = [
       {
-        class: "fa fa-twitter-square",
+        stackClass: "fa fa-square fa-stack-2x",
+        stackColor: "#3A5795",
+        iconClass: "fa fa-facebook fa-stack-1x",
+        iconColor: "#FFFFFF",
+        link: "https://www.facebook.com/archit.mahto"
+      },
+      {
+        stackClass: "fa fa-square fa-stack-2x",
+        stackColor: "#55ACEE",
+        iconClass: "fa fa-twitter fa-stack-1x",
+        iconColor: "#FFFFFF",
         link: "https://twitter.com/architMahto276"
       },
       {
-        class: "fa fa-linkedin-square",
+        stackClass: "fa fa-square fa-stack-2x",
+        stackColor: "#0077B5",
+        iconClass: "fa fa-linkedin fa-stack-1x",
+        iconColor: "#FFFFFF",
         link: "https://www.linkedin.com/in/architmahto"
       },
       {
-        class: "fa fa-github-square",
+        stackClass: "fa fa-circle fa-stack-2x",
+        stackColor: "#FFFFFF",
+        iconClass: "fa fa-github fa-stack-2x",
+        iconColor: "#000000",
         link: "https://github.com/architMahto"
       },
       {
-        class: "fa fa-stack-overflow",
+        stackClass: "fa fa-circle fa-stack-2x",
+        stackColor: "#FFA500",
+        iconClass: "fa fa-stack-overflow fa-stack-1x",
+        iconColor: "#FFFFFF",
         link: "https://careers.stackoverflow.com/architmahto"
       },
       {
-        class: "fa fa-envelope",
+        stackClass: "fa fa-envelope fa-stack-2x",
+        stackColor: "#FFFFFF",
+        iconClass: "fa fa-envelope-o fa-stack-2x",
+        iconColor: "#E93E30",
         link: "mailto:architmahto@gmail.com"
       }
     ];
